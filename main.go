308771dbeafb967dfb7767b0a6a1483adbfc1994
FrostyKitten02 <alen.fridau02@gmail.com>
2025-04-12 17:11:25 +0200

    init gamestate

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 type Game struct {
-	state GameState
+	state *GameState
 }
 
 func (g *Game) Update() error {
@@ -38,51 +38,11 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 
 func main() {
 	ebiten.SetWindowSize(1280, 720)
-	ebiten.SetWindowTitle("Hello, World!")
+	ebiten.SetWindowTitle("IS-sim!")
 	game := &Game{
-		state: GameState{
-			elements: []Element{
-				&Circle{
-					Location{
-						X: 800,
-						Y: 300,
-					},
-				},
-			},
-			agents: []Agent{
-				{
-					Location: &Location{
-						X: 40,
-						Y: 40,
-					},
-					SpeedVec: &SpeedVector{
-						X: 1,
-						Y: 0,
-					},
-				},
-				{
-					Location: &Location{
-						X: 0,
-						Y: 0,
-					},
-					SpeedVec: &SpeedVector{
-						X: 0,
-						Y: 1,
-					},
-				},
-				{
-					Location: &Location{
-						X: 0,
-						Y: 0,
-					},
-					SpeedVec: &SpeedVector{
-						X: 1,
-						Y: 1,
-					},
-				},
-			},
-		},
+		state: &GameState{},
 	}
+	game.state.InitGameState()
 
 	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
